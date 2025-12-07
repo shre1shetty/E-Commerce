6b59496612976d54b61cbfae1732b3f1bfcb3a5b
shre1shetty <shrevan506@gmail.com>
2025-12-07 20:02:49 +0530

    debug

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -2,13 +2,18 @@ import { AdminAxiosInstance } from "@/lib/AdminAxiosInstance";
 import { AdminAxiosInstanceforUpload } from "@/lib/AdminAxiosInstanceforUpload";
 import { AxiosInstance } from "@/lib/AxiosInstance";
 import { LS } from "@/lib/SecureLocalStorage";
-import React, { createContext, useLayoutEffect, useState } from "react";
+import React, {
+  createContext,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
 
 export const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
   const [accessToken, setaccessToken] = useState(null);
   const [role, setrole] = useState(null);
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (window.location.pathname !== import.meta.env.BASE_URL)
       AxiosInstance.post(
         "/User/refreshToken",
@@ -16,17 +21,11 @@ const AuthContextProvider = ({ children }) => {
         { withCredentials: true }
       )
         .then((res) => {
-          console.log(res.data.accessToken);
+          console.log(res.data);
           if (res.data.role === "admin") {
             setaccessToken(res.data.accessToken);
             setrole("admin");
             AdminAxiosInstance.defaults.headers.common[
-              "Authorization"
-            ] = `Bearer ${res.data.accessToken}`;
-            AdminAxiosInstance.defaults.headers.post[
-              "Authorization"
-            ] = `Bearer ${res.data.accessToken}`;
-            AdminAxiosInstance.defaults.headers.get[
               "Authorization"
             ] = `Bearer ${res.data.accessToken}`;
             AdminAxiosInstanceforUpload.defaults.headers.common[
